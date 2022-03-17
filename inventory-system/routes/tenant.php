484ee6b77ebbb80d13c53860d204e13150125101
Koh-0 <jwkoh5010@gmail.com>
2022-03-17 18:28:04 +0800

    kohjw

--- inventory-system/routes/tenant.php
+++ inventory-system/routes/tenant.php
@@ -26,4 +26,35 @@ Route::middleware([
     Route::get('/', function () {
         return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
     });
+    Route::get('/login', function () {
+        return view('login');
+    });
+    Route::get('/nav', function () {
+        return view('nav');
+    });
+    Route::get('/inventoryreport', function () {
+        return view('inventoryreport');
+    });
+    Route::get('/inventoryreportcustomize', function () {
+        return view('inventoryreportcustomize');
+    });
+    Route::get('/stock', function () {
+        return view('stock');
+    });
+    Route::get('/editstock', function () {
+        return view('editstock');
+    });
+    Route::get('/createstock', function () {
+        return view('createstock');
+    });
+    Route::get('/product', function () {
+        return view('product');
+    });
+    Route::get('/editproduct', function () {
+        return view('editproduct');
+    });
+    Route::get('/createproduct', function () {
+        return view('createproduct');
+    });
 });
+
